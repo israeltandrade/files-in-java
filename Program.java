ca160677e5a83ae94c1e1ab2ec32e90ad03803b9
israeltandrade <israeltandrade@hotmail.com>
2024-02-01 07:59:58 -0300

    FileWriter and BufferedWriter

--- Program.java
+++ Program.java
@@ -1,48 +1,28 @@
 package arquivos;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String filePath = "/mnt/SSD-1TB/Workspaces-Programacao/ws-eclipse/curso_programacao/src/arquivos/in.txt";
-		/*
-		 * // Declaração fica obsoleta com uso de "try with resources": FileReader
-		 * fileReader = null; BufferedReader bufferedReader = null;
-		 */
+		String[] fileLines = new String[] { "Good morning", "Good afternoon", "Good night" };
 
-		// Bloco "try with resources":
-		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
-			String line = bufferedReader.readLine();
-
-			while (line != null) {
-				System.out.println(line);
-				line = bufferedReader.readLine();
+		String filePath = "/mnt/SSD-1TB/Workspaces-Programacao/ws-eclipse/curso_programacao/src/arquivos/out.txt";
+		
+		// Objeto FileWriter sem o segundo parâmetro com valor true RECRIA o arquivo
+		// ao invés de acrescentar informação no mesmo (aqui acrescenta):
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath, true))) {
+			for (String fileLine : fileLines) {
+				bufferedWriter.write(fileLine);
+				bufferedWriter.newLine();
 			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		}
+		catch (IOException e) {
+			e.printStackTrace();
 		}
 		
-		// Bloco finally obsoleto com try with resources:
-		/*
-		 * finally { try { if (bufferedReader != null) { bufferedReader.close(); } if
-		 * (fileReader != null) { fileReader.close(); } }
-		 * 
-		 * catch (IOException e) { e.printStackTrace(); } }
-		 */
-
-		// Classe "File" permite acessar um arquivo e suas propriedades:
-		/*
-		 * File file = new File(
-		 * "/mnt/SSD-1TB/Workspaces-Programacao/ws-eclipse/curso_programacao/src/arquivos/in.txt"
-		 * ); Scanner sc = null; try { sc = new Scanner(file); while (sc.hasNextLine())
-		 * { System.out.println(sc.nextLine()); } } catch (IOException e) {
-		 * System.out.println("Error: " + e.getMessage()); } finally { if (sc != null) {
-		 * sc.close(); } }
-		 */
-
 	}
 }
